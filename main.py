5f2fe0249165fdaec7842418ca1f459adcfe39c4
Vjeran Grozdanic <vjeran.grozdanic@gmail.com>
2019-02-04 23:27:28 +0100

    D

--- main.py
+++ main.py
@@ -4,6 +4,75 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from googletrans import Translator
+
+#Functions
+'''
+---------------------------------
+=================================
+---------------------------------
+'''
+def translate(text, source="", destination=""):
+    translator = Translator();
+    translatedText=""
+    ERROR_TEXT = "Something went wrong!"
+
+    if (source == "" and destination == ""):
+        try: 
+            translatedText = translator.translate(text).text
+        except ValueError:
+            translatedText = ERROR_TEXT
+    
+    elif (source != "" and destination == ""):
+        try: 
+            translatedText = translator.translate(text, src=source).text
+        except ValueError:
+            translatedText = ERROR_TEXT
+    
+    else:
+        try: 
+            translatedText = translator.translate(text, src=source, dest=destination).text
+        except ValueError:
+            translatedText = ERROR_TEXT
+
+    return translatedText
+
+def parse(argument):
+    text=""
+    src=""
+    dest=""
+
+    try:
+        temp = -1
+        counter = 0
+        for i in range (len(argument)):
+            if (argument[i] == ":" and counter == 0):
+                dest += argument[i+1] + argument[i+2]
+                temp = i
+                counter = 1
+                for j in range (i+1, len(argument)):
+                    if (argument[j] == ":"):
+                        src += argument[j+1] + argument[j+2]
+                        temp = j
+            if (temp == -1 and argument[i] != " "):
+                temp = i-2
+                text += argument[i]
+
+            if (i > temp+2):
+                text += argument[i]
+            
+    except IndexError:
+        pass
+
+    return text, src, dest
+
+'''
+---------------------------------
+=================================
+---------------------------------
+'''
+
+# Class decleration
 class GoogleTranslateExtension(Extension):
 
     def __init__(self):
@@ -14,14 +83,13 @@ class GoogleTranslateExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
-                                             on_enter=HideWindowAction()
-                                             ))
-        return RenderResultListAction(items)
+        argument = event.get_argument()
+        text, src, dest = parse(argument)
+        translation = text + "|-|" + src + "|.|" + dest
+
+        return RenderResultListAction([ExtensionResultItem(icon='images/icon.png',
+                                                           name=translation,
+                                                           on_enter=HideWindowAction())])
 
         
 if __name__ == '__main__':
